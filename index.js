9340ed8b55f1c239fdc3576f1b601f1ba20b31e2
Denise Bagnell <dbagnell@gmail.com>
2025-07-17 12:31:44 -0400

    Refactored index.js

--- index.js
+++ index.js
@@ -1,39 +1,17 @@
 //index.js
-const {
-  fetchMyIP,
-  fetchCoordsByIP,
-  fetchISSFlyOverTimes
-} = require('./iss');
+const { nextISSTimesForMyLocation } = require('./iss');
 
-fetchMyIP((error, ip) => {
+const printPassTimes = function(passTimes) {
+  for (const pass of passTimes) {
+    const datetime = new Date(pass.risetime * 1000);
+    const duration = pass.duration;
+    console.log(`Next pass at ${datetime} for ${duration} seconds!`);
+  }
+};
+nextISSTimesForMyLocation((error, passTimes) => {
   if (error) {
-    console.log("It didn't work!", error);
-    return;
+    return console.log("It didn't work!", error);
   }
 
-  console.log('It worked! Returned IP:', ip);
-
-  fetchCoordsByIP(ip, (error, coords) => {
-    if (error) {
-      console.log("Failed to fetch coordinates", error);
-      return;
-    }
-
-    console.log("Coordinates are:", coords);
-
-    fetchISSFlyOverTimes(coords, (error, passes) => {
-      if (error) {
-        console.log("Failed to fetch ISS flyover times:", error);
-        return;
-      }
-      for (const pass of passes) {
-        const datetime = new Date(pass.risetime * 1000);
-        const duration = pass.duration;
-        console.log(`Next pass at ${datetime} for ${duration} seconds!`);
-      }
-    });
-  });
+  printPassTimes(passTimes);
 });
-// use request to fetch IP address from JSON API
-
-module.exports = { fetchMyIP };
